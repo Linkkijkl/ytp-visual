5fe326bfa26706862e211bbc022384bfdf710a7b
viljokass <vili.karkkainen@outlook.com>
2024-06-11 17:13:59 +0300

    added 3d ytp logo

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ monitor texture: Jami Virtanen
 
 import * as THREE from 'three';
 
-// If for some reason the loader is in a different place,
+// If for some reason the OBJLoader is in a different place,
 // try the another import. For me the one above worked on 
 // linux and the one below on windows.
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
@@ -21,18 +21,15 @@ var aspect = window.innerWidth / window.innerHeight;
 // to use when sceen changes size
 window.addEventListener( 'resize', resize_callback, false );
 function resize_callback(){
-
     aspect = window.innerWidth / window.innerHeight;
     camera.aspect = aspect;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
 
-
 // The three.js scene
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xffc1cc);
+const main_scene = new THREE.Scene();
+main_scene.background = new THREE.Color(0xffc1cc);
 
 // The three.js camera
 const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 100);
@@ -42,41 +39,39 @@ camera.position.y = 1;
 // The three.js renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.type = THREE.PCFShadowMap;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// The render target for camera 2, for the monitor
-var render_target = new THREE.WebGLRenderTarget(120, 90);
-render_target.texture.magFilter = THREE.NearestFilter;
-render_target.texture.minFilter = THREE.NearestFilter;
-var cam2 = new THREE.PerspectiveCamera(75, 4/3, 1, 100);
-cam2.position.set(5, 2, -5);
-cam2.lookAt(new THREE.Vector3(0, 1, 0));
+// The render target for camera 2, for the monitor screen
+var render_target = new THREE.WebGLRenderTarget(120, 90, {
+	magFilter: THREE.NearestFilter,
+	minFilter: THREE.NearestFilter
+});
 
 // LIGHTS
 var directionalLight1 = new THREE.DirectionalLight( 0xffff00, 1 );
 directionalLight1.position.set(-2,2,2);
 directionalLight1.castShadow = true;
-scene.add( directionalLight1 );
+main_scene.add( directionalLight1 );
 
 var directionalLight2 = new THREE.DirectionalLight( 0xff00ff, 1 );
 directionalLight2.position.set(2,2,-2);
 directionalLight2.castShadow = true;
-scene.add( directionalLight2 );
+main_scene.add( directionalLight2 );
 
 var directionalLight3 = new THREE.DirectionalLight( 0x00ffff, 1 );
 directionalLight3.position.set(-2,2,-2);
 directionalLight3.castShadow = true;
-scene.add( directionalLight3 );
+main_scene.add( directionalLight3 );
 
 var directionalLight4 = new THREE.DirectionalLight( 0xffffff, 1 );
 directionalLight4.position.set(2,2,2);
 directionalLight4.castShadow = true;
-scene.add( directionalLight4 );
+main_scene.add( directionalLight4 );
 
 var ambient = new THREE.AmbientLight(0xffc1cc, 1);
-scene.add(ambient);
+main_scene.add(ambient);
 
 // PLANE
 var planeGeometry = new THREE.PlaneGeometry( 20, 20, 1, 1 );
@@ -100,7 +95,7 @@ plane_material.onBeforeCompile = function(shader) {
 var plane = new THREE.Mesh( planeGeometry, plane_material );
 plane.receiveShadow = true;
 plane.rotation.x = -90 * dtr;
-scene.add( plane );
+main_scene.add( plane );
 
 // MONITOR
 var monitor_group = new THREE.Group();
@@ -118,8 +113,9 @@ monitor_group.add(monitor_power_light_L);
 
 // monitor screen materials
 // ytp-texture
+var jami_tex = new THREE.TextureLoader().load("jami.png");
 var monitor_material1 = new THREE.MeshBasicMaterial({
-    map: new THREE.TextureLoader().load("jami.png")
+    map: jami_tex,
 });
 
 // render target texutre (from cam2)
@@ -150,12 +146,12 @@ obj_loader.load('/monitori.obj',
             }
         });
         monitor_group.add(obj);
-        scene.add(monitor_group);
+        main_scene.add(monitor_group);
                 
         // monitor_group.rotation.y = 25*dtr;
         // monitor_group.position.set(-1.3, 0, -0.6);
         monitor_group.rotation.x = 6.5*dtr;
-        monitor_group.position.set(0, 0, -0.6);
+        monitor_group.position.set(0, 0, -0.5);
         monitor_loaded = true;
     },
     undefined,
@@ -183,7 +179,7 @@ obj_loader.load('pylvaes.obj',
             mesh.scale.set(0.4, 0.4, 0.4);
             mesh.receiveShadow = true;
             pylvaeaet.push(mesh);
-            scene.add(mesh);
+            main_scene.add(mesh);
         }
 	// Set the column positions
         pylvaeaet[0].position.set( 3.2, 1.95,  0);
@@ -199,50 +195,90 @@ obj_loader.load('pylvaes.obj',
         console.error(error);
     });
 
-var ukko_loaded = false;
-/*
-var ukko = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1), 
-    new THREE.MeshPhongMaterial({color: 0xffffff}));
-    
-    ukko.position.set(0, 1, 10);
-    scene.add(ukko);
-    cam2.lookAt(ukko.position)
-    ukko_loaded = true;
-*/
-var kolmio = false;
-var matswitch = 0.0;
+var logo_group = new THREE.Group();
+var kolmio_loaded = false;
+obj_loader.load('tri.obj',
+    function( obj ) {
+	var kolmio_material = new THREE.MeshPhongMaterial({color: 0xffff00});
+	obj.traverse( function (child) {
+	    if (child instanceof THREE.Mesh) {
+		child.material = kolmio_material;
+		child.receiveShadows = true;
+	    }
+	});
+	logo_group.add(obj);
+	kolmio_loaded = true;
+    },
+    undefined,
+    function(error) {
+	console.error(error);
+    });
+
+var ytp_texts_loaded = false;
+obj_loader.load('ytp.obj',
+    function ( obj ) {
+        var text_material = new THREE.MeshPhongMaterial({color: 0xff3f0f});
+	var text_geom;
+	obj.traverse(function(child) {
+	    if (child instanceof THREE.Mesh) {
+		text_geom = child.geometry;
+	    }
+	});
+	for (var i = 0; i < 2; i++) {
+	    let mesh = new THREE.Mesh(text_geom, text_material);
+	    mesh.scale.set(0.9, 0.9, 1)
+	    mesh.position.y = -0.1;
+	    mesh.rotation.y = 180 * dtr * i;
+	    mesh.castShadows = true;
+	    logo_group.add(mesh);
+	}
+	ytp_texts_loaded = true;
+    },
+    undefined,
+    function(error) {
+	console.error(error);
+    });
+
+// Create the monitor scene
+const monitor_scene = new THREE.Scene();
+//monitor_scene.background = new THREE.Color(0xffc1cc);
+monitor_scene.add(logo_group);
+logo_group.position.y = 0.05;
+var mon_sce_dir_lig = new THREE.DirectionalLight(0xffffff, 2);
+mon_sce_dir_lig.position.set(0, 1, 1);
+monitor_scene.add(mon_sce_dir_lig);
+var mon_sce_amb_lig = new THREE.AmbientLight(0xffc1cc);
+monitor_scene.add(mon_sce_amb_lig);
+var cam2 = new THREE.PerspectiveCamera(65, 4/3, 1, 100);
+cam2.position.set(0, 0, 2);
+
+var logo3d = false;
+var switch_time = 0.0;
 var render = function (time) {
     time = time/1000;
     requestAnimationFrame(render);
 
-    if (kolmio && time - matswitch > 4) {
+    if (logo3d && time - switch_time > pi) {
         monitor_screen.material = monitor_material1;
-        matswitch = time;
-        kolmio = false;
-    } else if (!kolmio && time - matswitch > 10) {
+        switch_time = time;
+        logo3d = false;
+    } else if (!logo3d && time - switch_time > 16) {
         monitor_screen.material = monitor_material3;
-        matswitch = time;
-        kolmio = true;
+        switch_time = time;
+        logo3d = true;
     }
     if (monitor_loaded) {
-        monitor_group.position.y = Math.sin(time/2)/6 + 0.30;
+        monitor_group.position.y = Math.sin(time/2)/6 + 0.35;
     }
-    if (ukko_loaded) {
-        ukko.rotation.y += 0.01;
-        ukko.rotation.x += 0.02;
+    if (kolmio_loaded && ytp_texts_loaded) {
+        logo_group.rotation.y = 2 * (time - switch_time);
     }
 
-    cam2.position.set(5+Math.sin(time/4)/4, 2,-5-Math.sin(time/4)/4);
 
     renderer.setRenderTarget(render_target);
-    renderer.render(scene, cam2);
+    renderer.render(monitor_scene, cam2);
     renderer.setRenderTarget(null);
-    renderer.render(scene, camera)
+    renderer.render(main_scene, camera)
 };
 
 render();
-
-
-
-
