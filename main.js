fe155eaa271932f4c3d7fb3bf7fe64e25be558c8
viljokass <vili.karkkainen@outlook.com>
2024-06-14 15:17:29 +0300

    added info text (to be modified)

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ import * as THREE from 'three';
 // linux and the one below on windows.
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 // import { OBJLoader } from 'three/addons/loaders/OBJloader.js';
+import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 
 // Degrees to radians.
 const pi = 3.1415;
@@ -33,8 +34,9 @@ main_scene.background = new THREE.Color(0xffc1cc);
 
 // The three.js camera
 const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 100);
-camera.position.z = 2.5;
-camera.position.y = 1;
+camera.position.z = 4;
+camera.position.y = 2;
+camera.rotation.x = -0 * dtr;
 
 // The three.js renderer
 const renderer = new THREE.WebGLRenderer();
@@ -45,7 +47,7 @@ document.body.appendChild(renderer.domElement);
 
 // The render target for camera 2, for the monitor screen
 var res = 30;
-var render_target = new THREE.WebGLRenderTarget(4 * res, 3 * res, {
+var render_target3D = new THREE.WebGLRenderTarget(4 * res, 3 * res, {
 	magFilter: THREE.NearestFilter,
 	minFilter: THREE.NearestFilter
 });
@@ -74,11 +76,42 @@ main_scene.add( directionalLight4 );
 var ambient = new THREE.AmbientLight(0xffc1cc, 1);
 main_scene.add(ambient);
 
+/*
+var sphere = new THREE.Mesh(
+	new THREE.SphereGeometry(1, 16, 16), 
+	new THREE.MeshPhongMaterial({
+	color: 0xFF69B4,
+	emissive: 0xFF69B4,
+	specular: 0XFF69B4,
+	shininess: 100,
+	}));
+sphere.castShadow = true;
+sphere.receiveShadow = true;
+sphere.position.set(2, 1, -4)
+main_scene.add(sphere);
+*/
+
+var info = new THREE.Mesh(
+	new THREE.PlaneGeometry(2, 1, 1, 1), 
+	new THREE.MeshBasicMaterial({
+	    map: new THREE.TextureLoader().load("info.png", {
+		    minFilter: THREE.NearestFilter,
+		    magFilter: THREE.NearestFilter,
+	        })
+	}));
+info.position.x = 2;
+info.position.z = 1.5;
+info.rotation.y = -8*dtr;
+main_scene.add(info);
+
 // PLANE
-var planeGeometry = new THREE.PlaneGeometry( 20, 20, 1, 1 );
+var plane_geometry = new THREE.PlaneGeometry( 20, 20, 1, 1 );
 // Spoof texture so that uv's are in use
 const spoof = new THREE.Texture();
-var plane_material = new THREE.MeshPhongMaterial({map: spoof});
+var plane_material = new THREE.MeshPhongMaterial({
+	map: spoof,
+	shininess: 100,
+});
 // inject custom glsl (might want to improve)
 plane_material.onBeforeCompile = function(shader) {
     shader.fragmentShader = shader.fragmentShader.replace(
@@ -93,7 +126,7 @@ plane_material.onBeforeCompile = function(shader) {
         ].join( '\n' )
     );
 };
-var plane = new THREE.Mesh( planeGeometry, plane_material );
+var plane = new THREE.Mesh( plane_geometry, plane_material );
 plane.receiveShadow = true;
 plane.rotation.x = -90 * dtr;
 main_scene.add( plane );
@@ -121,13 +154,13 @@ var monitor_material1 = new THREE.MeshBasicMaterial({
 
 // render target texutre (from cam2)
 var monitor_material3 = new THREE.MeshBasicMaterial({
-    map: render_target.texture
+    map: render_target3D.texture
 });
 
 // Monitor screen
 var monitor_screen = new THREE.Mesh(
     new THREE.PlaneGeometry(1.5,1.2,1,1),
-    monitor_material1
+    monitor_material3
 );
 monitor_screen.position.set(0, 1.27, 0.67);
 monitor_screen.rotation.x = -8.2 * dtr;
@@ -149,10 +182,10 @@ obj_loader.load('/monitori.obj',
         monitor_group.add(obj);
         main_scene.add(monitor_group);
                 
-        // monitor_group.rotation.y = 25*dtr;
-        // monitor_group.position.set(-1.3, 0, -0.6);
-        monitor_group.rotation.x = 6.5*dtr;
-        monitor_group.position.set(0, 0, -0.5);
+        monitor_group.rotation.y = 25*dtr;
+        monitor_group.position.set(-2, 0, -1);
+        // monitor_group.rotation.x = 6.5*dtr;
+        // monitor_group.position.set(0, 0, -0.5);
         monitor_loaded = true;
     },
     undefined,
@@ -175,20 +208,24 @@ obj_loader.load('pylvaes.obj',
             }
         });
 	// Add meshes created of the geometry and the material to the column array
-        for (var i = 0; i < 6; i++) {
+        for (var i = 0; i < 8; i++) {
             var mesh = new THREE.Mesh(pylvaes_geom, pylvaes_material);
-            mesh.scale.set(0.4, 0.4, 0.4);
+            mesh.scale.set(0.5, 0.5, 0.5);
             mesh.receiveShadow = true;
             pylvaeaet.push(mesh);
             main_scene.add(mesh);
         }
+	let h = 1.95 * 5/4;
 	// Set the column positions
-        pylvaeaet[0].position.set( 3.2, 1.95,  0);
-        pylvaeaet[1].position.set( 3.2, 1.95, -2);
-        pylvaeaet[2].position.set( 3.2, 1.95, -4);
-        pylvaeaet[3].position.set(-3.2, 1.95,  0);
-        pylvaeaet[4].position.set(-3.2, 1.95, -2);
-        pylvaeaet[5].position.set(-3.2, 1.95, -4);
+        pylvaeaet[0].position.set( 5, h,  0.5);
+        pylvaeaet[1].position.set( 5, h, -2  );
+        pylvaeaet[2].position.set( 5, h, -4.5);
+	pylvaeaet[3].position.set( 5, h, -7  );
+        pylvaeaet[4].position.set(-5, h,  0.5);
+        pylvaeaet[5].position.set(-5, h, -2  );
+        pylvaeaet[6].position.set(-5, h, -4.5);
+        pylvaeaet[7].position.set(-5, h, -7  );
+	
         pylvaes_loaded = true;
     },
     undefined,
@@ -243,8 +280,11 @@ obj_loader.load('ytp.obj',
 // Create the monitor scene
 const monitor_scene = new THREE.Scene();
 monitor_scene.background = new THREE.Color(0xffc1cc);
+
+// LOGO
 monitor_scene.add(logo_group);
 logo_group.position.y = 0.05;
+// MONITOR SCENE LIGHTS
 var mon_sce_dir_lig = new THREE.DirectionalLight(0xffffff, 2);
 mon_sce_dir_lig.position.set(0, 1, 1);
 monitor_scene.add(mon_sce_dir_lig);
@@ -252,13 +292,19 @@ var mon_sce_amb_lig = new THREE.AmbientLight(0xffc1cc);
 monitor_scene.add(mon_sce_amb_lig);
 var cam2 = new THREE.PerspectiveCamera(65, 4/3, 1, 100);
 cam2.position.set(0, 0, 2);
+// MONITOR SCENE PLANE
+var mon_plane = new THREE.Mesh(plane_geometry, plane_material);
+mon_plane.rotation.x = -90 * dtr;
+mon_plane.scale.set(10, 10);
+mon_plane.position.y = -25;
+monitor_scene.add(mon_plane);
 
 var logo3d = false;
 var switch_time = 0.0;
 var render = function (time) {
     time = time/1000;
     requestAnimationFrame(render);
-
+/*
     if (logo3d && time - switch_time > pi) {
         monitor_screen.material = monitor_material1;
         switch_time = time;
@@ -272,12 +318,14 @@ var render = function (time) {
     if (monitor_loaded) {
         monitor_group.position.y = Math.sin(time/2)/6 + 0.35;
     }
+*/
     if (kolmio_loaded && ytp_texts_loaded) {
         logo_group.rotation.y = -2 * (time - switch_time);
     }
+    info.position.y = Math.sin(time)/4 + 2;
 
 
-    renderer.setRenderTarget(render_target);
+    renderer.setRenderTarget(render_target3D);
     renderer.render(monitor_scene, cam2);
     renderer.setRenderTarget(null);
     renderer.render(main_scene, camera)
