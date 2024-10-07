fc2ab682d8849e25b87a6456f630296c9e0aa7bc
Lari Nurkka <lari.nurkka@gmail.com>
2024-10-08 01:15:14 +0300

    Map buttons

--- map.js
+++ map.js
@@ -1,5 +1,9 @@
+const defaultLocation = [62.23802, 25.74483];
+const defaultZoomLevel = 13;
+
 window.addEventListener("DOMContentLoaded", () => {
-    const map = L.map("map").setView([51.505, -0.09], 13);
+    const map = L.map("map", { zoomControl: false, zoomAnimation: false })
+            .setView(defaultLocation, defaultZoomLevel);
     let marker, circle, zoomed;
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,23 +11,33 @@ window.addEventListener("DOMContentLoaded", () => {
         attribution: '© OpenStreetMap'
     }).addTo(map);
 
-    navigator.geolocation.watchPosition((pos) => {
-        const lat = pos.coords.latitude;
-        const lng = pos.coords.longitude;
-        const accuracy = pos.coords.accuracy;
-    
-        if (marker) {
-            map.removeLayer(marker);
-            map.removeLayer(circle);
-        }
+    document.querySelector("#map button.locate").addEventListener("click", () => {
+        navigator.geolocation.watchPosition((pos) => {
+            const lat = pos.coords.latitude;
+            const lng = pos.coords.longitude;
+            const accuracy = pos.coords.accuracy;
+        
+            if (marker) {
+                map.removeLayer(marker);
+                map.removeLayer(circle);
+            }
 
-        marker = L.marker([lat, lng]).addTo(map);
-        circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
+            marker = L.marker([lat, lng]).addTo(map);
+            circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
 
-        if (!zoomed) {
-            zoomed = map.fitBounds(circle.getBounds()); 
-        }
+            if (!zoomed) {
+                zoomed = map.fitBounds(circle.getBounds()); 
+            }
 
-        map.setView([lat, lng]);
-    }, null);
+            map.setView([lat, lng]);
+        }, null);
+    });
+
+    document.querySelector("#map button.zoom-in").addEventListener("click", () => {
+        map.zoomIn(1);
+    });
+
+    document.querySelector("#map button.zoom-out").addEventListener("click", () => {
+        map.zoomOut(1);
+    });
 });
