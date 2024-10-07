67f5a05bac6ca31f297edb795fce6c383c4b44bb
Lari Nurkka <lari.nurkka@gmail.com>
2024-10-08 02:08:25 +0300

    Map markers

--- map.js
+++ map.js
@@ -1,15 +1,36 @@
 const defaultLocation = [62.23802, 25.74483];
 const defaultZoomLevel = 13;
+const markers = {
+    "Agora" : {
+        //"icon": ??
+        "pos": [62.23219, 25.73691],
+        "info": "Luennot luennoidaan täällä."
+    },
+    "Aalto-sali": {
+        "pos": [62.24351, 25.74986],
+        "info": "Etkot etkoillaan täällä."
+    }
+}
 
 window.addEventListener("DOMContentLoaded", () => {
-    const map = L.map("map", { zoomControl: false, zoomAnimation: false })
-            .setView(defaultLocation, defaultZoomLevel);
-    let marker, circle, zoomed;
+    const map = L.map("map", { zoomControl: false, zoomAnimation: false, minZoom: 11 })
+        .setView(defaultLocation, defaultZoomLevel);
+    let locationMarker, locationCircle, zoomed;
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '© OpenStreetMap'
     }).addTo(map);
+
+    for (markerName of Object.keys(markers)) {
+        const marker = markers[markerName];
+        const gmapsLink = `https://www.google.com/maps/place/${marker.pos.join(",")}`
+        const info = `${marker.info ? marker.info + "<br/>" : ""}<a href="${gmapsLink}" target="_blank" rel="noopener noreferrer">Google Maps</a>`
+        L.marker(marker.pos, {
+            //icon: ??
+            title: markerName,
+        }).bindPopup(info).addTo(map);
+    }
 
     document.querySelector("#map button.locate").addEventListener("click", () => {
         navigator.geolocation.watchPosition((pos) => {
@@ -17,16 +38,16 @@ window.addEventListener("DOMContentLoaded", () => {
             const lng = pos.coords.longitude;
             const accuracy = pos.coords.accuracy;
         
-            if (marker) {
-                map.removeLayer(marker);
-                map.removeLayer(circle);
+            if (locationMarker) {
+                map.removeLayer(locationMarker);
+                map.removeLayer(locationCircle);
             }
 
-            marker = L.marker([lat, lng]).addTo(map);
-            circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
+            locationMarker = L.marker([lat, lng]).addTo(map);
+            locationCircle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
 
             if (!zoomed) {
-                zoomed = map.fitBounds(circle.getBounds()); 
+                zoomed = map.fitBounds(locationCircle.getBounds()); 
             }
 
             map.setView([lat, lng]);
